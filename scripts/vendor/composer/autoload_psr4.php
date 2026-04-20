160333ff1c5246848e3307a09143bc7ab67350b1
hotriluan <hotriluan@gmail.com>
2026-04-20 12:59:52 +0700

    chore(deps): add PHP testing dependencies via composer
    - Add PHPUnit and related testing packages
    - Add composer autoload configuration
    - Include php-code-coverage, mockery, and static analysis tools

--- scripts/vendor/composer/autoload_psr4.php
+++ scripts/vendor/composer/autoload_psr4.php
@@ -8,8 +8,11 @@ $baseDir = dirname($vendorDir);
 return array(
     'ZipStream\\' => array($vendorDir . '/maennchen/zipstream-php/src'),
     'Psr\\SimpleCache\\' => array($vendorDir . '/psr/simple-cache/src'),
+    'PhpParser\\' => array($vendorDir . '/nikic/php-parser/lib/PhpParser'),
     'PhpOffice\\PhpSpreadsheet\\' => array($vendorDir . '/phpoffice/phpspreadsheet/src/PhpSpreadsheet'),
     'Matrix\\' => array($vendorDir . '/markbaker/matrix/classes/src'),
+    'DeepCopy\\' => array($vendorDir . '/myclabs/deep-copy/src/DeepCopy'),
     'Composer\\Pcre\\' => array($vendorDir . '/composer/pcre/src'),
     'Complex\\' => array($vendorDir . '/markbaker/complex/classes/src'),
+    'AlkanaTests\\' => array($baseDir . '/../tests/toolkit'),
 );
